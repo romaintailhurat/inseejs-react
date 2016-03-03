a981454c9a1a978ac00e6adf818d57a731a625cb
romaintailhurat <romain.tailhurat@gmail.com>
2016-03-03 13:15:20 +0100

    Adding webpack to the picture

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,17 @@
+var path = require("path");
+
 module.exports = {
+  entry: {
+    app: ["./main.js"]
+  },
   module: {
     loaders: [
       { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
     ]
+  },
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/assets/",
+    filename: "bundle.js"
   }
 }
