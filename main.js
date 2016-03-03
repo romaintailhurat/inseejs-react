bb1362ebad761d0239c4b080d2da4863dc941b32
romaintailhurat <romain.tailhurat@gmail.com>
2016-03-03 16:11:41 +0100

    Setting up Webpack to reload app on saving

--- main.js
+++ main.js
@@ -1,4 +1,22 @@
-import { range } from './utils'
+import { range } from './utils';
 
-console.log('WORKS');
-console.log('range 10', range(10));
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+console.log(`Chargement de l'application [${new Date().toTimeString()}]`);
+console.log('Test de la fonction range(10) >> ', range(10));
+
+
+// TODO React app entry point.
+class App extends React.Component {
+
+  render() {
+    return (
+        <h1>{this.props.titre}</h1>
+    );
+  }
+}
+
+App.propTypes = { titre: React.PropTypes.string };
+
+ReactDOM.render(<App titre="l e i f" />, document.getElementById('app'));
--- webpack.config.js
+++ webpack.config.js
@@ -10,8 +10,7 @@ module.exports = {
     ]
   },
   output: {
-    path: path.resolve(__dirname, "build"),
-    publicPath: "/assets/",
+    path: __dirname,
     filename: "bundle.js"
   }
 }
