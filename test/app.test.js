8615c18b71a149a2a28dca80c8f8a7971055e4b0
romaintailhurat <romain.tailhurat@gmail.com>
2016-04-11 14:48:08 +0200

    Ajout de tests et des frameworks qui vont bien

--- test/app.test.js
+++ test/app.test.js
@@ -1,2 +1,21 @@
 'use strict';
 let assert = require('assert');
+let listStore = require('../src/js/stores/stores.js').listStore;
+let RECEIVE_NAF = require('../src/js/actions/actions.js').RECEIVE_NAF;
+
+const DUMMY_NAF_DATA = { key: "value" };
+
+describe('Store', function() {
+  it('doit ne rien faire si l\'action n\'est pas connue', function() {
+    let stateBefore = listStore.getState();
+    listStore.dispatch({ type: 'NOTHING', data: null });
+    let stateAfter = listStore.getState();
+    assert.equal(stateBefore, stateAfter);
+  });
+
+  it('doit modifier l\'état', function() {
+    listStore.dispatch({ type: RECEIVE_NAF, data: DUMMY_NAF_DATA});
+    let stateAfter = listStore.getState();
+    assert.equal(stateAfter.naf, DUMMY_NAF_DATA);
+  });
+});
