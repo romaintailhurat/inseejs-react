960a01a646addf7b613d3a8cd6532c3125575c8b
romaintailhurat <romain.tailhurat@gmail.com>
2016-05-27 17:29:17 +0200

    Refacto Welcome, pure fonction pour un composant React stateless

--- src/js/components/Welcome.js
+++ src/js/components/Welcome.js
@@ -1,17 +1,14 @@
 import React from 'react';
 
-export default class Welcome extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.titre}</h1>
-        <h2>{this.props.soustitre}</h2>
-      </div>
-    );
-  }
-}
+const Welcome = (props) =>
+  <div>
+    <h1>{props.titre}</h1>
+    <h2>{props.soustitre}</h2>
+  </div>;
 
 Welcome.propTypes = {
   titre: React.PropTypes.string,
   soustitre: React.PropTypes.string,
 };
+
+export default Welcome;
