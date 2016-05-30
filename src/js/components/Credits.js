4e8a7cc4a443170ab3fa46370a0038054a7a6e89
romaintailhurat <romain.tailhurat@gmail.com>
2016-05-30 14:51:35 +0200

    Ajout d'un Footer et d'une vue Credits =D

---
+++ src/js/components/Credits.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Credits = () =>
+  <div>
+    <ul>
+      <li>Olivier Levitt</li>
+      <li>Romain Tailhurat</li>
+    </ul>
+  </div>;
+
+export default Credits;
---
+++ src/js/components/Footer.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router';
+
+const Footer = () => <div id="footer"><Link to="/credits">Credits</Link></div>;
+
+export default Footer;
