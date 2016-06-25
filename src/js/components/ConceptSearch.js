de0a553092799bc2c26aab5b2782a61f5647df1f
romaintailhurat <romain.tailhurat@gmail.com>
2016-06-25 11:31:23 +0200

    Meilleure usage du composant de complétion

--- src/js/components/ConceptSearch.js
+++ src/js/components/ConceptSearch.js
@@ -1,6 +1,30 @@
 import React from 'react';
 import Autocomplete from 'react-autocomplete';
 
+const CONCEPTS =
+  [
+    'Age',
+    'Chômage',
+    'Croissance',
+    'Ménage',
+    'PIB',
+    'Prix',
+    'Taxe',
+    'TVA'];
+
+function sortConcepts(a, b, value) {
+  return (
+    b.toLowerCase().indexOf(value.toLowerCase()) >
+    a.toLowerCase().indexOf(value.toLowerCase()) ? 1 : -1
+  );
+}
+
+function shouldRender(concept, value) {
+  return (
+    concept.toLowerCase().indexOf(value.toLowerCase()) !== -1
+  )
+}
+
 class ConceptSearch extends React.Component {
   constructor(props) {
     super(props);
@@ -13,11 +37,18 @@ class ConceptSearch extends React.Component {
         <p>Recherche de concepts</p>
         <Autocomplete
           value={this.state.value}
-          items={['TVA', 'Chômage', 'Taxe', 'Croissance', 'PIB', 'Prix', 'Age', 'Ménage']}
+          items={CONCEPTS}
+          inputProps={{ name: 'Concepts', id: 'concepts-autocomplete' }}
           getItemValue={(item) => item}
           onChange={(event, value) => this.setState({ value })}
           onSelect={value => this.setState({ value })}
-          renderItem={(item) => <div>{item}</div>}
+          sortItems={sortConcepts}
+          shouldItemRender={shouldRender}
+          renderItem={(item) =>
+            <div key={item}>
+              {item}
+            </div>
+          }
         />
       </div>
     );
